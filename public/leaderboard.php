84aa7d08a4e1d6295924d293be69ab7659b43234
Annasisa01 <37231591+Annasisa01@users.noreply.github.com>
2024-05-21 17:45:45 -0500

    Update leaderboard.php

--- public/leaderboard.php
+++ public/leaderboard.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "root","zambisa_warrior");
+    $conn = mysqli_connect("us-cluster-east-01.k8s.cleardb.net", "b697a2f19bb9ee", "c81af3f0","heroku_a42b3f930ee7d0c");
     $query = "SELECT * FROM `players`  \n"
 
     . "ORDER BY `players`.`highscore`  DESC LIMIT 5";
