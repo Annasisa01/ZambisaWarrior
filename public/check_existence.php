6ccc58e92a80cbbc69c7804a119cbfc24342b301
Annasisa01 <37231591+Annasisa01@users.noreply.github.com>
2024-05-21 17:45:02 -0500

    Update check_existence.php

--- public/check_existence.php
+++ public/check_existence.php
@@ -1,7 +1,7 @@
 <?php
     if (isset($_POST['name'])) {
         $name = $_POST['name'];
-        $conn = mysqli_connect("localhost", "root", "root","zambisa_warrior");
+        $conn = mysqli_connect("us-cluster-east-01.k8s.cleardb.net", "b697a2f19bb9ee", "c81af3f0","heroku_a42b3f930ee7d0c");
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
