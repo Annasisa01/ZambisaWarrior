032136857dd5a53eca092072a4e14ff0f2ba2129
Annasisa01 <37231591+Annasisa01@users.noreply.github.com>
2024-06-17 07:55:08 -0500

    Update check_existence.php

--- public/check_existence.php
+++ public/check_existence.php
@@ -2,9 +2,11 @@
     if (isset($_POST['name'])) {
         $name = $_POST['name'];
         $conn = mysqli_connect("us-cluster-east-01.k8s.cleardb.net", "b697a2f19bb9ee", "c81af3f0","heroku_a42b3f930ee7d0c");
+        echo 'here';
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
+        echo 'here2';
         $query = "SELECT * FROM players WHERE name = '$name'";
         $result = mysqli_query($conn,$query);
         $arr = array();
